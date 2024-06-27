c580f71b21cc7b8caa8b72776cdaecadadede64c
parnia1384 <168190104+parnia1384@users.noreply.github.com>
2024-06-28 03:11:19 +0330

    Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         GameController run = new GameController();
+        run.getInformationFromFile();
+        System.out.println("If you don't know what to do, type \"command prompt\".");
         run.run();
+        run.writeInformationInFile();
     }
 }
